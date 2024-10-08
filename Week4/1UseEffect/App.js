58c82563b71ab9de02bec3b42bd765a65bd9c981
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2024-10-08 12:03:20 -0700

    updated

--- Week4/1UseEffect/App.js
+++ Week4/1UseEffect/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import IntroToEffect from "./components/IntroToEffect";
+import SimpleAPICall from "./components/SimpleAPICall";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <IntroToEffect />
+      {/* <IntroToEffect /> */}
+      <SimpleAPICall />
       <StatusBar style="auto" />
     </View>
   );
