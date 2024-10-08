20dc949d152e2b5322520abd9c44de48a252663d
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2024-10-08 11:16:50 -0700

    added web

---
+++ Week4/1UseEffect/components/IntroToEffect.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function IntroToEffect() {
+  return (
+    <View>
+      <Text>IntroToEffect</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
