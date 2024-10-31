0b5c05ae8733120f3f6da0f36db4099f138ba85a
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2024-10-31 11:57:50 -0700

    Updated

--- Week7/DeviceApi/screens/StorageScreen.js
+++ Week7/DeviceApi/screens/StorageScreen.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 //AsyncStorage
 
 //super similar to localStorage in the dom
@@ -12,9 +13,41 @@ import React, { useState } from "react";
 export default function StorageScreen() {
   const [input, setInput] = useState("");
   const [savedText, setSavedText] = useState("");
+
+  //async function that saves text
+  const saveText = async () => {
+    try {
+      //save the input variable
+      await AsyncStorage.setItem("savedText", input);
+      setSavedText(input);
+      setInput("");
+    } catch (error) {
+      console.error(`Error Saving ${error}`);
+    }
+  };
+  //async function that loads text
+  const loadSavedText = async () => {
+    try {
+      //load a value from AsyncStorage
+      const value = await AsyncStorage.getItem("savedText");
+      if (value !== null) {
+        setSavedText(value);
+      }
+    } catch (error) {
+      console.error(`Error Loading ${error}`);
+    }
+  };
   return (
     <View>
-      <Text>StorageScreen</Text>
+      <Text>AsyncStorage Demo</Text>
+      <TextInput
+        value={input}
+        placeholder="Enter Text to save"
+        onChangeText={setInput}
+      />
+      <Button title="Save Text" onPress={saveText}></Button>
+      <Text>Saved Text: {savedText}</Text>
+      <Button title="Load Saved Text" onPress={loadSavedText}></Button>
     </View>
   );
 }
